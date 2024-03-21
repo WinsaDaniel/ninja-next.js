f592f8ad0a1f05634b8929f8517e89b45609755b
WinsaDaniel <winsadaniel7@gmail.com>
2024-03-21 16:13:51 +0300

    next.js fixations

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,60 +1,50 @@
 import { notFound } from "next/navigation";
 
 export const dynamicParams = true;
-
 export async function generateStaticParams() {
   const res = await fetch("http://localhost:4000/tickets");
+
   const tickets = await res.json();
 
-  return tickets.map((ticket) => ({
-    params: {
-      id: ticket.id.toString(),
-    },
+  return;
+  tickets.map((ticket) => ({
+    id: ticket.id,
   }));
 }
 
 async function getTicket(id) {
-  // Imitate a delay in fetching data
+  //imitate
   await new Promise((resolve) => setTimeout(resolve, 3000));
-
   const res = await fetch("http://localhost:4000/tickets/" + id, {
-    headers: {
-      // Add any required headers here
+    next: {
+      revalidate: 60,
     },
   });
-
-  if (!res.ok) {
-    // Handle 404 error by throwing an error
-    throw new Error("Ticket not found");
-  }
 
   return res.json();
 }
 
-async function TicketDetails({ params }) {
-  try {
-    const ticket = await getTicket(params.id);
+if (!res.ok) {
+  notFound();
+}
 
-    return (
-      <main>
-        <nav className="card">
-          <h2>Ticket Details</h2>
-        </nav>
-        <div className="card">
-          <h3>{ticket.title}</h3>
-          <small>Created by {ticket.user_email}</small>
-          <p>{ticket.body}</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+async function TicketDetails({ params }) {
+  const ticket = await getTicket(params.id);
+  return (
+    <main>
+      <nav className="card">
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority}priority
         </div>
-      </main>
-    );
-  } catch (error) {
-    // Handle any errors, e.g., display a generic error message
-    console.error("Error fetching ticket details:", error);
-    return <div>Error fetching ticket details. Please try again later.</div>;
-  }
+      </div>
+    </main>
+  );
 }
 
 export default TicketDetails;
